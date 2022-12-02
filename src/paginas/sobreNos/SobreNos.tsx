1fb7ad6a9954a65f37b28a7b7f9aa0b35c3c1aec
Mentalize <117479674+projetomentalize@users.noreply.github.com>
2022-12-02 16:44:26 -0300

    Update SobreNos.tsx

--- src/paginas/sobreNos/SobreNos.tsx
+++ src/paginas/sobreNos/SobreNos.tsx
@@ -1,33 +1,19 @@
+import { Grid, Box, Typography } from "@material-ui/core";
 import React from "react";
-import  './Home.css'
-import {Grid, Button, Typography} from '@material-ui/core'
-import {Box} from '@mui/material'
+import './SobreNos.css'
 
-function Home(){
-    return(
+function SobreNos() {
+    return (
         <>
-            <Grid container direction="row" justifyContent="center" alignItems="center" className='caixa '>
-                <Grid alignItems="center" item xs={6}>
-                    <Box paddingX={20} >
-                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" className='titulo mtop' >Seja bem vindo(a)!</Typography>
-                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" className='titulo' >Aproveite para explorar o site!</Typography>
+            <Grid container direction='row' justifyContent='center' alignContent='center'>
+                <Box className='txtdark'>
+                    <Box paddingTop={1}>
+                        <Typography variant="h6" align='center'> Missão</Typography>
+                        <Typography variant="h6" align='center' gutterBottom > Conectar pessoas que precisam de apoio psicológico com profissionais da área dispostos a ajudar essas pessoas de forma gratuita. Nós promovemos saúde e bem estar para população brasileira.</Typography>
                     </Box>
-                </Grid>
-                <Grid xs={12} className='postagens'>
-                </Grid>
+                </Box>
             </Grid>
-            <Grid container direction="row" justifyContent="center" alignItems="center" className='caixa2' >
-                <Grid item xs={6}>
-                    <Box>
-                        <Typography variant="h4">Encontre usuarios que estão passando por situações similiares para que possam ajudar um ao outro e profissionais dispostos à ajudar.</Typography>
-                        <Typography></Typography>
-                    </Box>
-                </Grid>
-                <Grid item xs={4} className='caixa2'>
-                    <img src="https://img.freepik.com/vetores-gratis/ilustracao-em-vetor-conceito-abstrato-meetup-online-chamada-em-conferencia-juntar-se-ao-grupo-meetup-servico-online-de-video-chamada-comunicacao-a-distancia-reuniao-informal-metafora-abstrata-de-rede-de-membros_335657-2920.jpg?w=2000" alt="" width="500px" height="500px"></img>
-                </Grid>
-            </Grid>
-</>
+        </>
     );
 }
-
+export default SobreNos;
