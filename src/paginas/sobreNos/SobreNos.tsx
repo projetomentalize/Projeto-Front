647c0c94701dab2d68e0becc3c805b18e35c3404
Tadyêllo Bastos <tadyello@hotmail.com>
2022-12-11 22:35:42 -0300

    Up

--- src/paginas/sobreNos/SobreNos.tsx
+++ src/paginas/sobreNos/SobreNos.tsx
@@ -1,4 +1,4 @@
-import { Grid, Box, Typography, Container } from "@material-ui/core";
+import { Grid, Box, Typography } from "@material-ui/core";
 import React from "react";
 import NavbarTeste from "../../components/estaticos/navbarteste/NavbarTeste";
 import './SobreNos.css'
