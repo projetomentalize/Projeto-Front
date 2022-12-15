096ac60d2f6b6b8547cfbf7ffa9267a7c5a5dbf3
tadyello <tadyello@hotmail.com>
2022-12-15 12:19:51 -0300

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -15,10 +15,10 @@ import CadastroTema from './components/temas/cadastroTema/CadastroTema';
 import DeletarPostagem from './components/postagens/deletarPostagem/DeletarPostagem';
 import DeletarTema from './components/temas/deletarTema/DeletarTema';
 import { Provider } from 'react-redux';
-import store from './store/store';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Home2 from './paginas/home2/Home2';
+import store from './store/store';
 
 function App() {
   return (
