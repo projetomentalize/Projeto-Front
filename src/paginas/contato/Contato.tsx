bc126e4897262b49c9ae1b2ace8854e9d60d13f8
gabrielsjobs <112710128+gabrielsjobs@users.noreply.github.com>
2022-12-21 16:21:58 -0300

    Update Contato.tsx

--- src/paginas/contato/Contato.tsx
+++ src/paginas/contato/Contato.tsx
@@ -11,22 +11,22 @@ function Contato() {
         <>
         <NavbarTeste/>
         <Grid container direction='column' justifyContent='center' alignContent='center' className="container-contato">
-                <Box text-align='center'>
-                     <Typography variant='h5' color='textPrimary' component='h5' align='center' className="titulo-contato">CONTATE-NOS</Typography>
-                </Box>
-                <Box paddingX={1}  boxShadow={2} className='box-contato'>                    
-                    <form  className='form-contato'>
-                        <TextField id='nome' label='Nome' variant='outlined' name='nome' margin='normal' className="campos" size='small' fullWidth />
-                        <TextField id='usuario' label='E-mail' variant='outlined' name='usuario' margin='normal' className="campos" size='small' fullWidth />
-                        <TextareaAutosize id='mensagem' aria-label='Mensagem' aria-variant='outlined' name='mensagem' aria-margin='normal' defaultValue='Deixe aqui sua mensagem. Será um prazer atendê-lo.;)' className='txt-area'/>
-                        <TextField id="select" label='Categoria' variant='outlined' name='categoria' margin='normal' className="campos" size='small' fullWidth select>
-                            <MenuItem value="duvidas">Dúvidas</MenuItem>
-                            <MenuItem value="sugestoes">Sugestões</MenuItem>
-                        </TextField>
-                        <Box marginTop={2} textAlign='center'>
-                            <Button type='submit' variant='contained' color='primary' className='btnCadastrar'>
-                                Enviar
-                            </Button>
+                    <Box text-align='center'>
+                        <Typography variant='h5' color='textPrimary' component='h5' align='center' className="titulo-contato">CONTATE-NOS</Typography>
+                    </Box>
+                    <Box paddingX={1} boxShadow={2} className='box-contato'>
+                        <form className='form-contato'>
+                            <TextField id='nome' label='Nome' variant='outlined' name='nome' margin='normal' className="campos" size='small' fullWidth />
+                            <TextField id='usuario' label='E-mail' variant='outlined' name='usuario' margin='normal' className="campos" size='small' fullWidth />
+                            <TextField id="select" label='Categoria' variant='outlined' name='categoria' margin='normal' className="campos" size='small' fullWidth select>
+                                <MenuItem value="duvidas">Dúvidas</MenuItem>
+                                <MenuItem value="sugestoes">Sugestões</MenuItem>
+                            </TextField>
+                            <TextareaAutosize id='mensagem' aria-label='Mensagem' name='mensagem' className='txt-area' rows="8"/>
+                                <Box marginTop={2} textAlign='center'>
+                                    <Button type='submit' variant='contained' color='primary' className='btnCadastrar'>
+                                        Enviar
+                                    </Button>
                         </Box> 
                     </form>
                 </Box>
