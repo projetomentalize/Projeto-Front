81c3ec65b33dd70e00537805ba511eac495d75bb
gabrielsjobs <112710128+gabrielsjobs@users.noreply.github.com>
2022-12-21 16:10:15 -0300

    Update SobreNos.tsx

--- src/paginas/sobreNos/SobreNos.tsx
+++ src/paginas/sobreNos/SobreNos.tsx
@@ -1,15 +1,101 @@
-import { Grid, Box, Typography } from "@material-ui/core";
+import { Divider } from "@material-ui/core";
+import { Box, Grid } from "@mui/material";
 import React from "react";
 import NavbarTeste from "../../components/estaticos/navbarteste/NavbarTeste";
-import Home2 from "../home2/Home2";
 import './SobreNos.css'
 
-function SobreNos() {
-    return (
-        <>
-        <NavbarTeste/>
-        <Home2/>
-        </>
-    );        
-}
+        function SobreNos() {
+            return (
+                <>
+           <NavbarTeste />
+              {/* <Grid container direction='row' justifyContent='center' alignContent='center'  className="container-home1">
+                    <Grid item text-align='center' xs={12}>
+                            <Typography variant='h3' color='textPrimary' component='h3' align='center' className="titulo-aboutus">NOSSO DESAFIO ODS-3</Typography>
+                    </Grid>
+                    <Grid item xs={6}>
+                    <img src='https://raw.githubusercontent.com/projetomentalize/midia/47c25cf7b4bc6f17f24b63a0cceec625be1b3b3f/Imagem1.svg' alt="" className="img-aboutus1" ></img>
+                    </Grid>
+                    <Grid item xs={6}>    
+                       <Box>              
+                            <Typography align="center" className="txt-home1">No projeto integrador da Generation Brasil nos foi proposto o desafio desenvolver uma solução (site ou e-commerce) para o "Obejetivo 3" dos 17 OSD(Objetivos de Desenvolvimento Sustentável) da ONU.</Typography>
+                            <Typography align="center" className="txt-home1">O desafio do ODS3 é :
+                            "Como será que podemos contribuir para garantir o acesso a saúde de
+                            qualidade e/ou promover o bem-estar para todos, em todas as idades, em sua comunidade, cidade ou país?"</Typography>
+                           <Typography align="center" className="txt-home1">
+                           Escolhemos  criar uma rede social com o objetivo de atender a Meta 3.4 da OSD 3:
+                           Até 2030, reduzir em um terço a mortalidade prematura por doenças não transmissíveis via prevenção e tratamento, promover a saúde mental e o bem-estar, a saúde do trabalhador e da trabalhadora, e prevenir o suicídio, alterando significativamente a tendência de aumento.</Typography>
+                        </Box>
+                    </Grid>
+               </Grid>
+               <Divider/>
+               <Grid container direction='row' justifyContent='center' alignContent='center' className="container-home2" >
+                    <Grid item text-align='center' xs={12}>
+                            <Typography variant='h3' color='textPrimary' component='h3' align='center' className="titulo-aboutus">INDICADOR</Typography>
+                    </Grid>          
+                    <Grid item xs={6}>
+                    <img src='https://raw.githubusercontent.com/projetomentalize/midia/about-us/Imagem5.png' alt="" className="img-indicador1" ></img>
+                    </Grid>  
+                    <Grid item xs={6}>
+                    <img src='https://raw.githubusercontent.com/projetomentalize/midia/47c25cf7b4bc6f17f24b63a0cceec625be1b3b3f/Imagem2.svg' alt="" className="img-indicador2" ></img>
+                    </Grid>            
+
+               </Grid>
+
+               <Divider/>
+               <Grid container direction='row' justifyContent='center' alignContent='center' className="container-home2" >
+                    <Grid item text-align='center' xs={12}>
+                            <Typography variant='h3' color='textPrimary' component='h3' align='center' className="titulo-aboutus">PROJETO</Typography>
+                    </Grid>          
+                    <Grid item xs={4}>
+                    <img src='https://raw.githubusercontent.com/projetomentalize/midia/083a2b09d0e5d5408c9ffa65c5a341c2ea0ed357/img2.svg' width={200} height={200} alt="" className="img-3" ></img>
+                    <Typography variant='h4' color='textPrimary' component='h4' align='center' className="titulo-aboutus">PROBLEMA</Typography>
+                    </Grid>  
+                    <Grid item xs={4}>
+                    <img src='https://raw.githubusercontent.com/projetomentalize/midia/47c25cf7b4bc6f17f24b63a0cceec625be1b3b3f/Imagem3.svg'width={200} height={200} alt="" className="img-2" ></img>
+                    <Typography variant='h4' color='textPrimary' component='h4' align='center' className="titulo-aboutus">SOLUÇÃO</Typography>
+                    </Grid>
+                    <Grid item xs={4}>
+                    <img src='https://raw.githubusercontent.com/projetomentalize/midia/47c25cf7b4bc6f17f24b63a0cceec625be1b3b3f/Imagem4.svg'width={250} height={200} alt="" className="img-3" ></img>
+                    <Typography variant='h4' color='textPrimary' component='h4' align='center' className="titulo-aboutus">IMPACTO NA SOCIEDADE</Typography>
+                    </Grid>
+                                 
+
+               </Grid>
+
+                <Divider />
+                <Box text-align='center'>
+                    <img src='https://raw.githubusercontent.com/projetomentalize/midia/5326276f26c5657a2547bf7655e2fa45dbb9c106/2.svg' alt="" ></img>
+                </Box>
+                <Divider />
+                <Box text-align='center'>
+                    <img src='https://raw.githubusercontent.com/projetomentalize/midia/5326276f26c5657a2547bf7655e2fa45dbb9c106/3.svg' alt="" ></img>
+                </Box>
+                <Divider />
+                <Box text-align='center'>
+                    <img src='https://raw.githubusercontent.com/projetomentalize/midia/5326276f26c5657a2547bf7655e2fa45dbb9c106/4.svg' alt="" ></img>
+                </Box>
+                <Divider /> */}
+                <Grid container direction='column' justifyContent='center' alignContent='center' className="container-contato">
+
+                    <Box text-align='center'>
+                        <img src='https://raw.githubusercontent.com/projetomentalize/midia/749ca4a55d25c1d83e1e01293a6394983f2b3b0a/1.svg' alt="" ></img>
+                    </Box>
+                <Divider />
+                    <Box text-align='center'>
+                        <img src='https://raw.githubusercontent.com/projetomentalize/midia/5326276f26c5657a2547bf7655e2fa45dbb9c106/2.svg' alt="" ></img>
+                    </Box>
+                <Divider />
+                    <Box text-align='center'>
+                        <img src='https://raw.githubusercontent.com/projetomentalize/midia/5326276f26c5657a2547bf7655e2fa45dbb9c106/3.svg' alt="" ></img>
+                    </Box>
+                <Divider />
+                    <Box text-align='center'>
+                        <img src='https://raw.githubusercontent.com/projetomentalize/midia/5326276f26c5657a2547bf7655e2fa45dbb9c106/4.svg' alt="" ></img>
+                    </Box>
+                <Divider />
+                </Grid>
+                </>
+            );  
+        }     
+    
 export default SobreNos;
