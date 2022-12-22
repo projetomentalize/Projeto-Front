c4035ceb6764dc90779a6ce9a7dabae47da2a9cb
Renan <90129083+Renanbarsan@users.noreply.github.com>
2022-12-22 10:47:30 -0300

    Update NavbarExterna.tsx

--- src/components/estaticos/navbarexterna/NavbarExterna.tsx
+++ src/components/estaticos/navbarexterna/NavbarExterna.tsx
@@ -10,9 +10,11 @@ function NavbarExterna() {
         <>
             <AppBar position="fixed" className='navbar'>
                 <Toolbar className='fundo' variant="dense" >
+                    <Link to="/home2">
                     <Box className='logo' >
                         <img src="https://raw.githubusercontent.com/projetomentalize/midia/5b75364a92009a3098609fe8fa0b46298edb1dc1/logosf.svg" alt='logoMentalize' />
                     </Box>
+                    </Link>
                     <Box display="flex" justifyContent="end">
                         <Box mx={1} className='item'>
                             <Link to='/'>
@@ -35,7 +37,14 @@ function NavbarExterna() {
                                 </Typography>
                             </Link>
                         </Box>
-                        <Box mx={1} className='item'>
+                        <Box>
+                          <Link to="/cadastrousuario">
+                          <Typography className='texto' variant="h6" color="inherit">
+                            Cadastro
+                          </Typography>
+                          </Link>
+                        </Box>
+                        <Box mx={1} ml={73} className='item'>
                                 <ModalLogin />
                         </Box>
 
