a67d5af6e04c3aa45c8cfd5d721d8e37f40c422a
Renan <90129083+Renanbarsan@users.noreply.github.com>
2022-12-22 12:16:41 -0300

    Update Navbar.tsx

--- src/components/estaticos/navbar/Navbar.tsx
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import { AppBar, Button, Toolbar, Typography } from '@material-ui/core';
 import { Box } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom'
@@ -9,14 +9,18 @@ import { useDispatch } from "react-redux";
 import { toast } from 'react-toastify';
 import { addToken } from '../../../store/tokens/actions';
 import { TokenState } from '../../../store/tokens/tokensReducer';
+import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
+
 
 function Navbar(): JSX.Element {
+    const dispatch = useDispatch();
+
+    let navigate = useNavigate();
+
     const token = useSelector<TokenState, TokenState["tokens"]>(
         (state) => state.tokens
     );
-    let navigate = useNavigate();
-    const dispatch = useDispatch();
-
+    
     function goLogout() { 
         dispatch(addToken(''));
         toast.info('Usuário deslogado', {
@@ -37,14 +41,16 @@ function Navbar(): JSX.Element {
     if (token != "") {
         navbarComponent = <AppBar position="static">
             <Toolbar className='fundo' variant="dense">
+            <Link to="/home">
                     <Box className='logo' >
                         <img src="https://raw.githubusercontent.com/projetomentalize/midia/5b75364a92009a3098609fe8fa0b46298edb1dc1/logosf.svg" alt='logoMentalize' />
                     </Box>
+                    </Link>
                 <Box display="flex" justifyContent="start">
                     <Link to="/home" className="text-decorator-none">
                         <Box mx={1} className='cursor'>
                             <Typography variant="h6" color="inherit">
-                                Home
+                                Pagina Inicial
                             </Typography>
                         </Box>
                     </Link>
@@ -68,13 +74,29 @@ function Navbar(): JSX.Element {
                                 Cadastrar Tema
                             </Typography>
                         </Box>
-                    </Link>
-                    <Box mx={1} className='cursor' onClick={goLogout}>
+                    </Link>                   
+                    <Link to="/usuarios" className="text-decorator-none">
+                    <Box mx={1} className="cursor">
                         <Typography variant="h6" color="inherit">
-                            Logout
+                          Usuários
                         </Typography>
-                    </Box>
+                   </Box>
+                   </Link>
+                   <Link to="/perfil" className="text-decorator-none">
+                    <Box mx={1} className="cursor">
+                        <Typography variant="h6" color="inherit">
+                          Perfil
+                        </Typography>
+                   </Box>
+                   </Link>
                 </Box>
+                <Box mx={1} ml={50} className='cursor' justifyContent="flex-end" alignContent="flex-start">
+                    <Button 
+                     variant="contained"
+                     className="btnModal-logout"
+                      endIcon={<LogoutOutlinedIcon />}
+                      onClick={goLogout}> SAIR </Button>
+                      </Box>
             </Toolbar>
         </AppBar>
     }
